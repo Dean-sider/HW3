6e08f5a6a91addb2c0e8a9be13e634a8d316905b
Starmy <mf99250@gmail.com>
2020-04-30 21:19:28 +0800

    Initial Commit

---
+++ HW3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW3
+//
+//  Created by User19 on 2020/4/30.
+//  Copyright © 2020 ntou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
